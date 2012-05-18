8ad9d3b844de14d3cefcf97caa9469aaa9810cc0
Joseph Rouff <rouffj@gmail.com>
2012-05-18 10:55:50 +0200

    Add base classes

---
+++ lib/Rouffj/Time/CalendarDate.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * CalendarDate
+ *
+ * @author Joseph Rouff <rouffj@gmail.com>
+ */
+class CalendarDate
+{
+    
+    public static function from($year, $month, $day)
+    {
+        echo 'test';
+    }
+}
+
+CalendarDate::from(2012, 1, 1);
---
+++ lib/Rouffj/Time/TimePoint.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Rouffj\Time;
+
+/**
+ * @author Joseph Rouff <rouffj@gmail.com>
+ */
+class TimePoint
+{
+}
