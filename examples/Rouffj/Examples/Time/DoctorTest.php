0764704a4af75364a68b9bf1ab2ece4bdba96e66
Jean-François Simon <contact@jfsimon.fr>
2012-09-24 22:00:03 +0200

    Almost fixed examples

--- examples/Rouffj/Examples/Time/DoctorTest.php
+++ examples/Rouffj/Examples/Time/DoctorTest.php
@@ -3,17 +3,18 @@
 namespace Rouffj\Examples\Time;
 
 use Rouffj\Tests\TestCase;
-use Rouffj\Time\Core\TimePoint;
-use Rouffj\Time\Calendar\Calendar;
-use Rouffj\Time\Calendar\Event;
-use Rouffj\Time\Factory\TimeIntervalFactory;
-use Rouffj\Time\Factory\DateIntervalFactory;
+use Rouffj\Time\Domain\Model\Core\TimePoint;
+use Rouffj\Time\Domain\Calendar\Calendar;
+use Rouffj\Time\Domain\Calendar\BaseStrategy;
+use Rouffj\Time\Domain\Model\Event\Event;
+use Rouffj\Time\Domain\Factory\TimeIntervalFactory;
+use Rouffj\Time\Domain\Factory\DateIntervalFactory;
 
 class DoctorTest extends TestCase
 {
     public function testHowToRetrieveAllMyAppointments()
     {
-        $calendar = new Calendar(new DoctorEventRepository());
+        $calendar = new Calendar(new BaseStrategy(), new DoctorEventRepository());
 
         $this->assertSame('Doctor Smith\'s appointments', $calendar->getTitle());
         $this->assertCount(3, $calendar);
@@ -21,14 +22,14 @@ class DoctorTest extends TestCase
 
     public function testHowToRetrieveMyAppointmentsForToday()
     {
-        $calendar = new Calendar(new DoctorEventRepository());
+        $calendar = new Calendar(new BaseStrategy(), new DoctorEventRepository());
         $today = DateIntervalFactory::create('2012-01-01', '2012-01-01');
         $this->assertCount(1, $calendar->between(TimeIntervalFactory::fromDateInterval($today)));
     }
 
     public function testHowToRetrieveAppointmentsAfterADate()
     {
-        $calendar = new Calendar(new DoctorEventRepository());
+        $calendar = new Calendar(new BaseStrategy(), new DoctorEventRepository());
         $today = new TimePoint(2012, 1, 1, 10, 0);
         $calendar->setCursor($today);
         $this->assertCount(2, $calendar);
@@ -36,15 +37,14 @@ class DoctorTest extends TestCase
 
     public function testHowToRetrieveMyAppointmentsForCurrentWeek()
     {
-        $calendar = new Calendar(new DoctorEventRepository());
+        $calendar = new Calendar(new BaseStrategy(), new DoctorEventRepository());
         $week = DateIntervalFactory::create('2012-01-01', '2012-01-07');
         $this->assertCount(2, $calendar->between(TimeIntervalFactory::fromDateInterval($week)));
     }
 
     public function testHowToAddNewAppointment()
     {
-        $repo = new DoctorEventRepository();
-        $calendar = new Calendar($repo);
+        $calendar = new Calendar(new BaseStrategy(), new DoctorEventRepository());
         $event = new Event(TimeIntervalFactory::create('2012-01-01 17:00', '2012-01-01 18:00'));
         $calendar->add($event);
         $this->assertCount(4, $calendar);
--- lib/Rouffj/Time/Domain/Service/EventPersisterInterface.php
+++ lib/Rouffj/Time/Domain/Service/EventPersisterInterface.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Rouffj\Time\Calendar;
+namespace Rouffj\Time\Domain\Service;
 
 use Rouffj\Time\Domain\Model\Event\EventInterface;
 
