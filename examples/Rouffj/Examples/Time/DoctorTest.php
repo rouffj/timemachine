673ef4be217763c23566d19a2442de807af99a9c
Joseph Rouff <rouffj@gmail.com>
2012-09-27 17:48:16 +0200

    Fix examples

--- examples/Rouffj/Examples/Time/DoctorTest.php
+++ examples/Rouffj/Examples/Time/DoctorTest.php
@@ -12,9 +12,15 @@ use TimeMachine\Time\Factory\DateIntervalFactory;
 
 class DoctorTest extends TestCase
 {
+    public function setup()
+    {
+        $this->repository = new DoctorEventRepository();
+        $this->calendar = new Calendar($this->repository->getName(), $this->repository->getEvents());
+    }
+
     public function testHowToRetrieveAllMyAppointments()
     {
-        $calendar = new Calendar(new BaseStrategy(), new DoctorEventRepository());
+        $calendar = $this->calendar;
 
         $this->assertSame('Doctor Smith\'s appointments', $calendar->getTitle());
         $this->assertCount(3, $calendar);
@@ -22,14 +28,16 @@ class DoctorTest extends TestCase
 
     public function testHowToRetrieveMyAppointmentsForToday()
     {
-        $calendar = new Calendar(new BaseStrategy(), new DoctorEventRepository());
+        $calendar = $this->calendar;
+
         $today = DateIntervalFactory::create('2012-01-01', '2012-01-01');
         $this->assertCount(1, $calendar->between(TimeIntervalFactory::fromDateInterval($today)));
     }
 
     public function testHowToRetrieveAppointmentsAfterADate()
     {
-        $calendar = new Calendar(new BaseStrategy(), new DoctorEventRepository());
+        $calendar = $this->calendar;
+
         $today = new TimePoint(2012, 1, 1, 10, 0);
         $calendar->setCursor($today);
         $this->assertSame(2, $calendar->countRemaining());
@@ -37,14 +45,16 @@ class DoctorTest extends TestCase
 
     public function testHowToRetrieveMyAppointmentsForCurrentWeek()
     {
-        $calendar = new Calendar(new BaseStrategy(), new DoctorEventRepository());
+        $calendar = $this->calendar;
+
         $week = DateIntervalFactory::create('2012-01-01', '2012-01-07');
         $this->assertCount(2, $calendar->between(TimeIntervalFactory::fromDateInterval($week)));
     }
 
     public function testHowToAddNewAppointment()
     {
-        $calendar = new Calendar(new BaseStrategy(), new DoctorEventRepository());
+        $calendar = $this->calendar;
+
         $event = new Event(TimeIntervalFactory::create('2012-01-01 17:00', '2012-01-01 18:00'));
         $calendar->add($event);
         $this->assertCount(4, $calendar);
