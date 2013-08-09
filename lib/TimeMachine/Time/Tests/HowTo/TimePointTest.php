33a7798c569897099dc6306a59b77635168d9037
deguipi <pierredamien.deguillebon@gmail.com>
2013-08-09 11:21:29 +0200

    Added the "how to" test functions into TimePointTest.php

--- lib/TimeMachine/Time/Tests/HowTo/TimePointTest.php
+++ lib/TimeMachine/Time/Tests/HowTo/TimePointTest.php
@@ -6,5 +6,39 @@ use TimeMachine\Time\Tests\TestCase;
 
 class TimePointTest extends TestCase
 {
+	public function testHowToKnowIfAItIsBeforeAfterEqualAnOtherTimePoint()
+	{
+
+	}
+
+	public function testHowToKnowIfAItIsBbeforeAfterEqualAGivenDate()
+	{
+
+	}
+
+	public function testHowToKnowIfAItIsBbeforeAfterEqualAGivenDateInterval()
+	{
+
+	}
+
+	public function testHowToKnowIfAItIsBbeforeAfterEqualAGivenTimeInterval()
+	{
+
+	}
+
+	public function testHowToKnowIfItIsDuringNightOrDaylight()
+	{
+
+	}
+
+	public function testHowToAddRemoveDurationFromIt()
+	{
+
+	}
+
+	public function testHowToConvertADddTimePointObjectIntoRegularDateTimeobject()
+	{
+
+	}
 
 }
