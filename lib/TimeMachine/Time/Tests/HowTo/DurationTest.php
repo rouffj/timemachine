e4bcc0f673e7779cc689370b8bd13b97968cbfb2
inalgnu <inal.djafar@sensiolabs.com>
2013-08-09 10:57:06 +0200

    Add duration test

--- lib/TimeMachine/Time/Tests/HowTo/DurationTest.php
+++ lib/TimeMachine/Time/Tests/HowTo/DurationTest.php
@@ -6,5 +6,33 @@ use TimeMachine\Time\Tests\TestCase;
 
 class DurationTest extends TestCase
 {
+    public function HowToKnowHowLongXDaysInHoursMinutesSecondsTest()
+    {
 
+    }
+
+    public function HowToKnowHowLongXWeeksInDaysHoursMinutesSecondsTest()
+    {
+
+    }
+
+    public function HowToKnowHowLongXMonthsInWeeksDaysHoursMinutesSecondsTest()
+    {
+
+    }
+
+    public function HowToKnowHowLongXYearsInMonthsWeeksDaysHoursMinutesSecondsTest()
+    {
+
+    }
+
+    public function HowToKnowHowLongXCenturiesInYearsMonthsWeeksDaysHoursMinutesSecondsTest()
+    {
+
+    }
+
+    public function HowToKnowHowLongAYearsBMonthsCWeeksDDaysInHoursMinutesSecondsTest()
+    {
+
+    }
 }
