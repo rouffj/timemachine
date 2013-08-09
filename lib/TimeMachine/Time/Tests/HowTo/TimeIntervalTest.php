f30449270142035ff4068c1034ce7d06dd8b3c2d
inalgnu <inal.djafar@sensiolabs.com>
2013-08-09 11:12:49 +0200

    [Time] Add time interval test

--- lib/TimeMachine/Time/Tests/HowTo/TimeIntervalTest.php
+++ lib/TimeMachine/Time/Tests/HowTo/TimeIntervalTest.php
@@ -6,5 +6,30 @@ use TimeMachine\Time\Tests\TestCase;
 
 class TimeIntervalTest extends TestCase
 {
+    public function testHowToKnowIfATimeIntervalIsBeforeAfterDuringAnOtherTimeInterval()
+    {
+    }
 
+    public function testHowToKnowIfATimeIntervalIsBeforeAfterDuringAGivenDateInterval()
+    {
+    }
+
+    public function testHowToKnowIfATimeIntervalIsBeforeAfterDuringAGivenDate()
+    {
+    }
+
+    public function testHowToKnowIfATimeIntervalIsBeforeAfterDuringAGivenDateTime()
+    {
+
+    }
+
+    public function testHowToTransformATimeIntervalIntoADateIntervalDestroyData()
+    {
+
+    }
+
+    public function testHowToKnowHowLongATimeIntervalAreInAtLeastSecondsMinutesHoursDaysWeeksYears()
+    {
+
+    }
 }
